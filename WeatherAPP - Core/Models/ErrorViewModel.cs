c365bda51e21d505a867c11a67ae635083990a31
Tomasz Bączek <tb054379@student.ath.edu.pl>
2021-04-19 13:15:29 +0200

    Dodaj pliki projektów.

---
+++ WeatherAPP - Core/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WeatherAPP___Core.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
