0a7304eb4f3b69dfd7da0cd29503e42ce2cbfd13
Michał <hanzel.michal1@gmail.com>
2021-04-27 21:40:33 +0200

    Przycisk działa , jeszcze brak wyświetlania danych

--- WeatherAPP - Core/Controllers/HomeController.cs
+++ WeatherAPP - Core/Controllers/HomeController.cs
@@ -6,6 +6,9 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using WeatherAPP___Core.Models;
+using WeatherForecast.Models;
+using Newtonsoft.Json;
+using System.Net;
 
 namespace WeatherAPP___Core.Controllers
 {
@@ -32,6 +35,45 @@ namespace WeatherAPP___Core.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        public String WeatherDetail(string City)
+        {
+
+            //Assign API KEY which received from OPENWEATHERMAP.ORG
+            string appId = "0770f1c5ab85fe7ddff0cf0e60b1efac";
+
+            //API path with CITY parameter and other parameters.
+            string url = string.Format("http://api.openweathermap.org/data/2.5/weather?q={0}&units=metric&cnt=1&APPID={1}", City, appId);
+
+            using (WebClient client = new WebClient())
+            {
+                string json = client.DownloadString(url);
+
+
+                RootObject weatherInfo = JsonConvert.DeserializeObject<RootObject>(json);
+
+
+                ResultViewModel rslt = new ResultViewModel();
+
+                rslt.Country = weatherInfo.sys.country;
+                rslt.City = weatherInfo.name;
+                rslt.Lat = Convert.ToString(weatherInfo.coord.lat);
+                rslt.Lon = Convert.ToString(weatherInfo.coord.lon);
+                rslt.Description = weatherInfo.weather[0].description;
+                rslt.Humidity = Convert.ToString(weatherInfo.main.humidity);
+                rslt.Temp = Convert.ToString(weatherInfo.main.temp);
+                rslt.TempFeelsLike = Convert.ToString(weatherInfo.main.feels_like);
+                rslt.TempMax = Convert.ToString(weatherInfo.main.temp_max);
+                rslt.TempMin = Convert.ToString(weatherInfo.main.temp_min);
+                rslt.WeatherIcon = weatherInfo.weather[0].icon;
+
+                //Converting OBJECT to JSON String 
+                var jsonstring = JsonConvert.SerializeObject(rslt, Formatting.Indented);
+
+
+                //Return JSON string.
+                return jsonstring;
+            }
         }
     }
 }
