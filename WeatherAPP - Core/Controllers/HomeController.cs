cf8becd9927ab4f9abb5b40a033e6f075dc7d694
Michał <hanzel.michal1@gmail.com>
2021-05-03 11:08:50 +0200

    Działający Projekt pokauzuje już pogodę

--- WeatherAPP - Core/Controllers/HomeController.cs
+++ WeatherAPP - Core/Controllers/HomeController.cs
@@ -1,30 +1,33 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using WeatherAPP___Core.Models;
-using WeatherForecast.Models;
-using Newtonsoft.Json;
-using System.Net;
+using WeatherAPP___Core.Services;
 
-namespace WeatherAPP___Core.Controllers
+namespace RagoWeather.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        public RestService _rs { get; set; }
+
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
+            _rs = new RestService();
         }
 
         public IActionResult Index()
         {
+            _logger.LogInformation("Index Request");
             return View();
         }
+
 
         public IActionResult Privacy()
         {
@@ -36,44 +39,13 @@ namespace WeatherAPP___Core.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
-        public String WeatherDetail(string City)
+
+        public IActionResult WeatherData(string id)
         {
+            string prova = "https://api.openweathermap.org/data/2.5/weather?q=+" + id + "&units=metric&appid=0770f1c5ab85fe7ddff0cf0e60b1efac";
+            WeatherData results = _rs.GetWeatherData(prova).Result;
 
-            //Assign API KEY which received from OPENWEATHERMAP.ORG
-            string appId = "0770f1c5ab85fe7ddff0cf0e60b1efac";
-
-            //API path with CITY parameter and other parameters.
-            string url = string.Format("http://api.openweathermap.org/data/2.5/weather?q={0}&units=metric&cnt=1&APPID={1}", City, appId);
-
-            using (WebClient client = new WebClient())
-            {
-                string json = client.DownloadString(url);
-
-
-                RootObject weatherInfo = JsonConvert.DeserializeObject<RootObject>(json);
-
-
-                ResultViewModel rslt = new ResultViewModel();
-
-                rslt.Country = weatherInfo.sys.country;
-                rslt.City = weatherInfo.name;
-                rslt.Lat = Convert.ToString(weatherInfo.coord.lat);
-                rslt.Lon = Convert.ToString(weatherInfo.coord.lon);
-                rslt.Description = weatherInfo.weather[0].description;
-                rslt.Humidity = Convert.ToString(weatherInfo.main.humidity);
-                rslt.Temp = Convert.ToString(weatherInfo.main.temp);
-                rslt.TempFeelsLike = Convert.ToString(weatherInfo.main.feels_like);
-                rslt.TempMax = Convert.ToString(weatherInfo.main.temp_max);
-                rslt.TempMin = Convert.ToString(weatherInfo.main.temp_min);
-                rslt.WeatherIcon = weatherInfo.weather[0].icon;
-
-                //Converting OBJECT to JSON String 
-                var jsonstring = JsonConvert.SerializeObject(rslt, Formatting.Indented);
-
-
-                //Return JSON string.
-                return jsonstring;
-            }
+            return View(results);
         }
     }
 }
