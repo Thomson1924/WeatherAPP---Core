92d08756287241fb89b957eb56cf90f564b2e0d7
Michał <hanzel.michal1@gmail.com>
2021-05-09 21:28:34 +0200

    Dodano widoki , na razie brak danych lecz już są wyświetlane.

--- WeatherAPP - Core/Controllers/HomeController.cs
+++ WeatherAPP - Core/Controllers/HomeController.cs
@@ -47,5 +47,19 @@ namespace RagoWeather.Controllers
 
             return View(results);
         }
+        public IActionResult HistoricalData(string id)
+        {
+            string prova = "https://api.openweathermap.org/data/2.5/weather?q=+" + id + "&lang=pl&units=metric&appid=0770f1c5ab85fe7ddff0cf0e60b1efac";
+            WeatherData results = _rs.GetWeatherData(prova).Result;
+
+            return View(results);
+        }
+        public IActionResult ForecastData(string id)
+        {
+            string prova = "https://api.openweathermap.org/data/2.5/weather?q=+" + id + "&lang=pl&units=metric&appid=0770f1c5ab85fe7ddff0cf0e60b1efac";
+            WeatherData results = _rs.GetWeatherData(prova).Result;
+
+            return View(results);
+        }
     }
 }
