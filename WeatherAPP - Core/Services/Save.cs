d32f354e4b5e850b5c0f1467d0695d194d34e009
przemekkamieniorz <przemekkamieniorz@PRZEMEKKAMIBB8D>
2021-05-11 15:09:33 +0200

    Stworzenie serwisu wykonującego operacje na bazie danych

---
+++ WeatherAPP - Core/Services/Save.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WeatherAPP___Core.Data;
+using WeatherAPP___Core.Interfaces;
+using WeatherAPP___Core.Models;
+
+namespace WeatherAPP___Core.Services
+{
+    public class Save : ISave
+    {
+        private readonly ApplicationDbContext _context;
+        public Save(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> SaveAsync(Main main)
+        {
+            
+            
+                _context.Add(main);
+               await _context.SaveChangesAsync();
+
+            return true;
+            
+        }
+    }
+}
