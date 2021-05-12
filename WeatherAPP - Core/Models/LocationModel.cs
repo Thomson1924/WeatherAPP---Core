d269d010db4a9727371f6196c386b655f80793cc
Tomasz Bączek <tb054379@student.ath.edu.pl>
2021-05-12 13:27:03 +0200

    Dodanie modelu danych współrzędnych lkoalizacji

---
+++ WeatherAPP - Core/Models/LocationModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WeatherAPP___Core.Models
+{
+    public class LocationModel
+    {
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+}
