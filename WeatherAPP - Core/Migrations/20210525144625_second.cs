7dedb8cbc4023c411e5a6866f5c50e13c3e0ffd5
przemekkamieniorz <przemekkamieniorz@PRZEMEKKAMIBB8D>
2021-05-25 17:03:57 +0200

    Utworzenie drugiej migracji dodajacek pole CityName do tabeli mains w bazie

---
+++ WeatherAPP - Core/Migrations/20210525144625_second.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WeatherAPP___Core.Migrations
+{
+    public partial class second : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CityName",
+                table: "mains",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CityName",
+                table: "mains");
+        }
+    }
+}
